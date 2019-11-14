886fb4d2ef9734a187a01e8dc2ff599ed0b763a8
domelon <mariusz@melonio.pl>
2019-11-14 21:59:12 +0100

    add-task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AddTaskComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
