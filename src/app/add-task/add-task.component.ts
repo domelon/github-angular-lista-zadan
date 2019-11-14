d13b3c2f860b0635b1a20380e0ecad919916e6d7
domelon <mariusz@melonio.pl>
2019-11-14 22:12:02 +0100

    2 files modified, 1 added

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-add-task',
-  templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  selector: "app-add-task",
+  templateUrl: "./add-task.component.html",
+  styleUrls: ["./add-task.component.css"]
 })
 export class AddTaskComponent implements OnInit {
+  newTask: string;
+  @Output()
+  emitTask = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  add() {
+    this.emitTask.emit(this.newTask);
+    this.newTask = "";
   }
-
 }
