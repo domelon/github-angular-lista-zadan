6449734d8c48edf5d42e8ea1d9be4369490e413f
domelon <mariusz@melonio.pl>
2019-11-14 21:56:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-angular-lista-zadan
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-angular-lista-zadan)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  newTask: string;
+  taskList: Array<string> = [];
+  taskDone: Array<string> = [];
+
+  add() {
+    this.taskList.push(this.newTask);
+    this.newTask = "";
+  }
+
+  remove(task: string) {
+    this.taskList = this.taskList.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.taskDone.push(task);
+    this.remove(task);
+  }
+}
